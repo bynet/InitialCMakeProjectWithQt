8e4b755deb636f40bdebc738c92a12962e204cb0
C.... Z <cihangirzengin@gmail.com>
2018-04-11 09:32:57 +0300

    Qt dll auto copy feature is added

--- Utils.cmake
+++ Utils.cmake
@@ -1,3 +1,20 @@
+macro(qt5_copy_dll APP DLL)
+    # find the release *.dll file
+    get_target_property(Qt5_${DLL}Location Qt5::${DLL} LOCATION)
+    # find the debug *d.dll file
+    get_target_property(Qt5_${DLL}LocationDebug Qt5::${DLL} IMPORTED_LOCATION_DEBUG)
+
+    add_custom_command(TARGET ${APP} POST_BUILD
+       COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<CONFIG:Debug>:${Qt5_${DLL}LocationDebug}> $<$<NOT:$<CONFIG:Debug>>:${Qt5_${DLL}Location}> $<TARGET_FILE_DIR:${APP}>)
+endmacro()
+
+macro(qt5_copy_dlls APP DLLs)
+
+  foreach(DLL ${DLLs})
+    qt5_copy_dll(${APP} ${DLL})
+  endforeach()
+
+endmacro()
 
 macro(parse_argument_list PREFIX KEYWORDS ARGUMENTS)
 	# Indirection over KEYWORD_LIST necessary because macro parameters are no real variables.
